2db56883f511cf97657bafffd883614855cc6d1a
Shubham123ssr <100044183+Shubham123ssr@users.noreply.github.com>
2022-09-14 00:19:40 +0530

    Add files via upload

---
+++ assignment 9/9.py
@@ -0,0 +1,80 @@
+#q1
+'''
+n=int(input("Enter the value of n "))
+i=1
+while i<=n:
+    print("MySirG")
+    i=i+1
+'''
+#q2
+'''
+n=int(input("Enter the value of n "))
+i=1
+while i<=n:
+    print(i)
+    i+=1
+'''
+#q3
+'''
+n=int(input("Enter the value of n "))
+i=n
+while i>0:
+    print(i)
+    i=i-1
+'''
+#q4
+'''
+n=int(input("Enter the value of n "))
+i=1
+while i<=2*n:
+    print(i)
+    i=i+2
+'''
+#q5
+'''
+n=int(input("Enter the value of n "))
+i=2*n-1
+while i>=1:
+    print(i)
+    i-=2
+'''
+#q6
+'''
+n=int(input("Enter the value of n "))
+i=2
+while i<=2*n:
+    print(i)
+    i+=2
+'''
+#q7
+'''
+n=int(input("Enter the value of n "))
+i=2*n
+while i>=2:
+    print(i)
+    i-=2
+'''
+#q8
+'''
+n=int(input("Enter the value of n "))
+i=1
+while i<=n:
+    print(i*i)
+    i+=1
+'''
+#q9
+'''
+n=int(input("Enter the value of n "))
+i=1
+while i<=n:
+    print(i*i*i)
+    i+=1
+'''
+#q10
+'''
+n=int(input("Enter the value of n "))
+i=1
+while i<=10:
+    print(n*i)
+    i+=1
+'''
